1164ccbdd5e0215edf8094f6768d10a080fe63ca
JefferDevop <somadesarrollo@gmail.com>
2023-09-09 14:19:30 -0500

    primeros cambios

--- products/models.py
+++ products/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.text import slugify
 from django.urls import reverse
+
 # from simple_history.models import HistoricalRecords
 from cloudinary.models import CloudinaryField
 from django_tenants.utils import get_public_schema_name
@@ -12,86 +13,102 @@ global_schema_name = None
 from django.shortcuts import render
 
 
-
 class Product(models.Model):
     codigo = models.BigAutoField(
-        primary_key=True, auto_created=True, verbose_name=(u'Código'))
-    name_extend = models.CharField(max_length=200, unique=True,
-                                   verbose_name=(u'Nombre Producto'))    
+        primary_key=True, auto_created=True, verbose_name=("Código")
+    )
+    name_extend = models.CharField(
+        max_length=200, unique=True, verbose_name=("Nombre Producto")
+    )
 
-    images = CloudinaryField('categories/', blank=True,  transformation=[{'width': 800, 'height': 1200, 'crop': 'limit'}, {'quality': 'auto'}], 
-                            format='webp')
+    images = CloudinaryField(
+        "categories/",
+        blank=True,
+        transformation=[
+            {"width": 800, "height": 1200, "crop": "limit"},
+            {"quality": "auto"},
+        ],
+        format="webp",
+    )
     description = models.TextField(
-        max_length=4000, blank=True, verbose_name=(u'Descripción el producto'))
+        max_length=4000, blank=True, verbose_name=("Descripción el producto")
+    )
     price1 = models.PositiveIntegerField(
-        blank=True, null=True, verbose_name=(u'Precio Detal'))
+        blank=True, null=True, verbose_name=("Precio Detal")
+    )
     price2 = models.PositiveIntegerField(
-        blank=True, null=True, verbose_name=(u'Precio por Mayor')) 
+        blank=True, null=True, verbose_name=("Precio por Mayor")
+    )
     price_old = models.PositiveIntegerField(
-        blank=True, null=True, verbose_name=(u'Precio Anterior'))
-    flag = models.CharField(max_length=200, blank=True, null=True,
-                            verbose_name=(u'Grupo'))
-    
-    ref = models.CharField(max_length=200, blank=True, null=True,
-                            verbose_name=(u'Referencia'))
-    
-    slug = models.SlugField(max_length=200, unique=True, verbose_name=(u'Url'))
-    
-    
-    active = models.BooleanField(default=True, verbose_name=(u'Activo'))
-    soldout = models.BooleanField(default=False, verbose_name=(u'Agotado'))
-    offer = models.BooleanField(default=False, verbose_name=(u'Oferta'))
-    home = models.BooleanField(default=False, verbose_name=(u'Exclusivo'))
-    created_date = models.DateTimeField(
-        auto_now_add=True, verbose_name=(u'Creado'))
-    modified_date = models.DateTimeField(
-        auto_now=True, verbose_name=(u'Modificado'))
+        blank=True, null=True, verbose_name=("Precio Anterior")
+    )
+    flag = models.CharField(
+        max_length=200, blank=True, null=True, verbose_name=("Grupo")
+    )
+
+    ref = models.CharField(
+        max_length=200, blank=True, null=True, verbose_name=("Referencia")
+    )
+
+    slug = models.SlugField(max_length=200, unique=True, verbose_name=("Url"))
+
+    active = models.BooleanField(default=True, verbose_name=("Activo"))
+    soldout = models.BooleanField(default=False, verbose_name=("Agotado"))
+    offer = models.BooleanField(default=False, verbose_name=("Oferta"))
+    home = models.BooleanField(default=False, verbose_name=("Exclusivo"))
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name=("Creado"))
+    modified_date = models.DateTimeField(auto_now=True, verbose_name=("Modificado"))
     # history = HistoricalRecords()
 
     class Meta:
-        verbose_name = 'Producto'
-        verbose_name_plural = 'Productos'
+        verbose_name = "Producto"
+        verbose_name_plural = "Productos"
 
     def __str__(self):
-        return f'{self.name_extend} : cod:{self.codigo}'    
+        return f"{self.name_extend} : cod:{self.codigo}"
+
 
 class Category(models.Model):
-    name = models.CharField(max_length=50, unique=True,
-                            verbose_name=(u'Nombre'))
-    slug = models.SlugField(max_length=100, unique=True, verbose_name=(u'Url'))
+    name = models.CharField(max_length=50, unique=True, verbose_name=("Nombre"))
+    slug = models.SlugField(max_length=100, unique=True, verbose_name=("Url"))
 
-    image = CloudinaryField('categories/',  blank=True, 
-                            transformation=[{'width': 800, 'height': 800, 'crop': 'limit'}, {'quality': 'auto'}], 
-                            format='webp')
-    created_date = models.DateTimeField(
-        auto_now_add=True, verbose_name=(u'Creado'))
-    modified_date = models.DateTimeField(
-        auto_now=True, verbose_name=(u'Modificado'))
+    image = CloudinaryField(
+        "categories/",
+        blank=True,
+        transformation=[
+            {"width": 800, "height": 800, "crop": "limit"},
+            {"quality": "auto"},
+        ],
+        format="webp",
+    )
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name=("Creado"))
+    modified_date = models.DateTimeField(auto_now=True, verbose_name=("Modificado"))
     # history = HistoricalRecords()
 
     class Meta:
-        verbose_name = 'Categoria'
-        verbose_name_plural = 'Categorias'
+        verbose_name = "Categoria"
+        verbose_name_plural = "Categorias"
 
     def get_url(self):
-        return reverse('products_by_category', args=[self.slug])
+        return reverse("products_by_category", args=[self.slug])
 
     def __str__(self):
         return self.name
-    
- 
+
+
 class CategoryProduct(models.Model):
     product = models.ForeignKey(
-        Product, on_delete=models.CASCADE, verbose_name=(u'Producto'))
+        Product, on_delete=models.CASCADE, verbose_name=("Producto")
+    )
     active = models.BooleanField(default=True)
-    created_date = models.DateTimeField(
-        auto_now_add=True, verbose_name=(u'Creado'))
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name=("Creado"))
     category = models.ForeignKey(
-        Category, on_delete=models.CASCADE, verbose_name=(u'Categoría'))
+        Category, on_delete=models.CASCADE, verbose_name=("Categoría")
+    )
 
     class Meta:
-        verbose_name = 'Categoria de Producto'
-        verbose_name_plural = 'Categoria de Productos'
+        verbose_name = "Categoria de Producto"
+        verbose_name_plural = "Categoria de Productos"
 
     def __str__(self):
         return str(self.category)
@@ -99,25 +116,25 @@ class CategoryProduct(models.Model):
 
 class Gallery(models.Model):
     product = models.ForeignKey(
-        Product, default=None, on_delete=models.CASCADE, verbose_name=(u'Producto'))
-    image = CloudinaryField('gallery', blank=True)
+        Product, default=None, on_delete=models.CASCADE, verbose_name=("Producto")
+    )
+    image = CloudinaryField("gallery", blank=True)
 
     def __str__(self):
         return str(self.product)
 
     class Meta:
-        verbose_name = 'Imagen'
-        verbose_name_plural = 'Galeria de Imagenes'
+        verbose_name = "Imagen"
+        verbose_name_plural = "Galeria de Imagenes"
 
 
 class Attribut(models.Model):
-    name = models.CharField(max_length=50, unique=True,
-                            verbose_name=(u'Nombre'))
+    name = models.CharField(max_length=50, unique=True, verbose_name=("Nombre"))
     # history = HistoricalRecords()
 
     class Meta:
-        verbose_name = 'Atributo'
-        verbose_name_plural = 'Atributos'
+        verbose_name = "Atributo"
+        verbose_name_plural = "Atributos"
 
     def __str__(self):
         return self.name
