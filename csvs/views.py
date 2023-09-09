2d8daf27b48b2950b15df65a3a77bd7dafff4b52
JefferDevop <somadesarrollo@gmail.com>
2023-09-09 02:02:47 -0500

    primeros cambios

--- csvs/views.py
+++ csvs/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render
 from .forms import CsvModelForm
+from .models import Csv
+import csv
+from sales.models import Sale
 # from django.http import HttpResponse
+
 
 def upload_file_view(request):
     form = CsvModelForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         form.save()
         form = CsvModelForm()
-    return render(request, 'csvs/upload.html', {'form': form})
+        obj = Csv.objects.get(activated=False)
+        with open(obj.file_name.path, 'r') as f:
+            reader = csv.reader(f)
+
+            for i, row in enumerate(reader):
+                if i==0:
+                    pass
+                else:
+                    row = "".join(row)     
+                    row = row.replace(";", " ")    
+                    row = row.split()            
+                    product = row[1].upper()
+                    Sale.objects.create(
+                        product = product,
+                        quantity = int(row[2]),
+                    )
+            obj.activated = True
+            obj.save()
+    return render(request, "csvs/upload.html", {"form": form})
