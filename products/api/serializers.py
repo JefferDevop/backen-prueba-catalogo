82770bd1d66aafa0a91e2f6f672a38628da734a3
JefferDevop <somadesarrollo@gmail.com>
2023-09-09 19:08:58 -0500

    pruebas de resultado

--- products/api/serializers.py
+++ products/api/serializers.py
@@ -5,35 +5,47 @@ from products.models import Gallery, Category, Product, CategoryProduct, Attribu
 class CategorySerializer(ModelSerializer):
     class Meta:
         model = Category
-        fields = ['id', 'name', 'slug', 'image']
-
+        fields = ["id", "name", "slug", "image", "image_alterna"]
 
 
 class AttributSerializer(ModelSerializer):
     class Meta:
         model = Attribut
-        fields = ['id', 'name']
-
+        fields = ["id", "name"]
 
 
 class ProductSerializer(ModelSerializer):
-   # data = CategoryProductSerializer(source='product_id', read_only=True, many=True)
+    # data = CategoryProductSerializer(source='product_id', read_only=True, many=True)
     class Meta:
         model = Product
-        fields = ['codigo', 'ref', 'flag', 'name_extend', 'slug', 'description', 'images',
-                  'price_old',  'price1', 'price2', 'active', 'soldout', 'offer', 'home']
+        fields = [
+            "codigo",
+            "ref",
+            "flag",
+            "name_extend",
+            "slug",
+            "description",
+            "images",
+            "price_old",
+            "price1",
+            "price2",
+            "active",
+            "soldout",
+            "offer",
+            "home",
+        ]
 
 
 class GallerySerializer(ModelSerializer):
     class Meta:
         model = Gallery
-        fields = ['id', 'product', 'image']
+        fields = ["id", "product", "image"]
 
 
 class CategoryProductSerializer(ModelSerializer):
-    categoryData = CategorySerializer(source='category', read_only=True)
-    productData = ProductSerializer(source='product', read_only=True)
+    categoryData = CategorySerializer(source="category", read_only=True)
+    productData = ProductSerializer(source="product", read_only=True)
 
     class Meta:
         model = CategoryProduct
-        fields = ['id', 'active', 'productData', 'categoryData']
+        fields = ["id", "active", "productData", "categoryData"]
