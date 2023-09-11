ac3fdc8b0af321809fe0598e3dec1d6771c8f9f4
JefferDevop <somadesarrollo@gmail.com>
2023-09-11 06:39:59 -0500

    pruebas de resultado

--- products/models.py
+++ products/models.py
@@ -67,7 +67,7 @@ class Product(models.Model):
         verbose_name_plural = "Productos"
 
     def __str__(self):
-        return f"{self.codigo}"
+        return {self.codigo}
 
 
 class Category(models.Model):
